9dea9659781ce2ea522cf1c042090fea1f9779bf
Martin Jul <martin@mjul.com>
2024-09-09 22:59:40 +0200

    Insert new parties

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use dotenvy::dotenv;
 use std::env;
 pub mod models;
 pub mod schema;
+use crate::models::{NewParty, Party};
 
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
@@ -12,21 +13,43 @@ pub fn establish_connection() -> SqliteConnection {
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
 
-fn main() {
-    println!("Hello, world!");
-
-    use self::models::Party;
+fn list_parties(conn: &mut SqliteConnection) {
     use self::schema::parties::dsl::*;
-
-    let mut conn: SqliteConnection = establish_connection();
     let results = parties
-        .limit(5)
+        .limit(10)
         .select(Party::as_select())
-        .load(&mut conn)
+        .load(conn)
         .expect("Error loading parties");
 
     println!("Parties (count={})", results.len());
     for party in results {
         println!("{}, {}", party.id, party.name);
     }
+}
+
+fn create_party(conn: &mut SqliteConnection, name: &str) -> Party {
+    use crate::schema::parties;
+
+    let new_party = NewParty { name };
+
+    diesel::insert_into(parties::table)
+        .values(&new_party)
+        .returning(Party::as_returning())
+        .get_result(conn)
+        .expect("Error saving new party")
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    use self::schema::parties::dsl::*;
+
+    let mut conn: SqliteConnection = establish_connection();
+
+    list_parties(&mut conn);
+
+    let buyer = create_party(&mut conn, "Bui Buyer");
+    let seller = create_party(&mut conn, "Shel Seller");
+
+    list_parties(&mut conn);
 }
--- src/models.rs
+++ src/models.rs
@@ -8,3 +8,10 @@ pub struct Party {
     pub id: i32,
     pub name: String,
 }
+
+/// A new [Party]
+#[derive(Insertable)]
+#[diesel(table_name = crate::schema::parties)]
+pub struct NewParty<'a> {
+    pub name: &'a str,
+}
